22023608cff5916be9edbd4ee4752640c8bfc037
aleniox <infinity.linhdv@gmail.com>
2025-07-21 08:27:39 +0700

    fix: Update model names and enhance smart agent functionality

--- modules/tools/agent_tools.py
+++ modules/tools/agent_tools.py
@@ -5,49 +5,49 @@ import modules.tools.tool_others as tool_others
 import json
 
 
-def smart_agent(user_message: str):
-    """Xử lý câu hỏi, quyết định dùng tool hay LLM trả lời trực tiếp"""
-    # Prompt để LLM phân tích câu hỏi
-    decision_prompt = f"""/no_think
-    Bạn là một trợ lý AI thông minh. Hãy phân tích câu hỏi sau và quyết định cách xử lý:
-    Bạn được cung cấp các công cụ sau:
-    - calender: Kiểm tra ngày tháng, thời gian, giờ, phút, giây
-    - direct_answer: nếu không cần thiết sử dụng công cụ nào, hãy trả lời trực tiếp bằng tiếng Việt.
-    - youtube_search: để tìm kiếm video hoặc nghe nhạc
-    - web_search: để tìm kiếm các thông tin từ internet
-    Trả lời ngắn gọn không được quyết định lung tung phải có căn cứ để trả lời.
-    Định dạng đầu ra:
+# def smart_agent(user_message: str):
+#     """Xử lý câu hỏi, quyết định dùng tool hay LLM trả lời trực tiếp"""
+#     # Prompt để LLM phân tích câu hỏi
+#     decision_prompt = f"""/no_think
+#     Bạn là một trợ lý AI thông minh. Hãy phân tích câu hỏi sau và quyết định cách xử lý:
+#     Bạn được cung cấp các công cụ sau:
+#     - calender: Kiểm tra ngày tháng, thời gian, giờ, phút, giây
+#     - direct_answer: nếu không cần thiết sử dụng công cụ nào, hãy trả lời trực tiếp bằng tiếng Việt.
+#     - youtube_search: để tìm kiếm video hoặc nghe nhạc
+#     - web_search: để tìm kiếm các thông tin từ internet
+#     Trả lời ngắn gọn không được quyết định lung tung phải có căn cứ để trả lời.
+#     Định dạng đầu ra:
 
-    {{"action": "direct_answer" | "calendar" | "youtube_search" | "web_search", "action_input": "câu query được sửa lại để sử dụng cho các công cụ"|""}}
-    Câu hỏi: "{user_message['content']}"
-    """
+#     {{"action": "direct_answer" | "calendar" | "youtube_search" | "web_search", "action_input": "câu query được sửa lại để sử dụng cho các công cụ"|""}}
+#     Câu hỏi: "{user_message['content']}"
+#     """
 
-    # Gọi LLM để quyết định hành động
-    decision = ollama.generate(model=config.MODEL_NAME_G, prompt=decision_prompt)
-    # .strip().lower()
-    print(f"Decision: {decision.response}")
-    # decision.response = decision.response.replace("<think>", "").replace("</think>", "").strip()
-    decision.response = tool_others.remove_think_content(decision.response)
-    if "direct_answer" in decision.response:
-        # Trả lời trực tiếp bằng LLM
-        return [user_message]
-    elif "calendar" in decision.response:
-        user_message = [{"role": "assistant", "content": tool_searchs.calendar_tool()}, user_message]
-        return user_message
-    elif "youtube_search" in decision.response:
-        # Tìm kiếm video trên Youtube
-        query = json.loads(decision.response)
-        youtube_results = tool_searchs.search_youtube(query['action_input'])
-        if not youtube_results:
-            return [{"role": "assistant", "content": "Không tìm thấy video phù hợp 😢"}, user_message]
-        youtube_results = "\n".join([f"""title: {r['title']} duration: {r['duration']} url: {r['url']}""" for r in youtube_results])
-        user_message = [{"role": "assistant", "content": f"Kết quả tìm kiếm: {youtube_results}"}, user_message]
-        return user_message
-    elif "web_search" in decision.response:
-        query = json.loads(decision.response)
-        web_results = tool_searchs.search_with_ddgs(query['action_input'])
-        user_message = [{"role": "assistant", "content": f"Kết quả tìm kiếm: {web_results}"}, user_message]
-        return user_message
+#     # Gọi LLM để quyết định hành động
+#     decision = ollama.generate(model=config.MODEL_NAME_G, prompt=decision_prompt)
+#     # .strip().lower()
+#     print(f"Decision: {decision.response}")
+#     # decision.response = decision.response.replace("<think>", "").replace("</think>", "").strip()
+#     decision.response = tool_others.remove_think_content(decision.response)
+#     if "direct_answer" in decision.response:
+#         # Trả lời trực tiếp bằng LLM
+#         return [user_message]
+#     elif "calendar" in decision.response:
+#         user_message = [{"role": "assistant", "content": tool_searchs.calendar_tool()}, user_message]
+#         return user_message
+#     elif "youtube_search" in decision.response:
+#         # Tìm kiếm video trên Youtube
+#         query = json.loads(decision.response)
+#         youtube_results = tool_searchs.search_youtube(query['action_input'])
+#         if not youtube_results:
+#             return [{"role": "assistant", "content": "Không tìm thấy video phù hợp 😢"}, user_message]
+#         youtube_results = "\n".join([f"""title: {r['title']} duration: {r['duration']} url: {r['url']}""" for r in youtube_results])
+#         user_message = [{"role": "assistant", "content": f"Kết quả tìm kiếm: {youtube_results}"}, user_message]
+#         return user_message
+#     elif "web_search" in decision.response:
+#         query = json.loads(decision.response)
+#         web_results = tool_searchs.search_with_ddgs(query['action_input'])
+#         user_message = [{"role": "assistant", "content": f"Kết quả tìm kiếm: {web_results}"}, user_message]
+#         return user_message
 
 def clean_excessive_newlines(text):
     """
@@ -68,9 +68,13 @@ def smart_agent_decision(user_message: str):
         print(tools.message.tool_calls)
         for tool in tools.message.tool_calls:
             if tool.function.name == 'calculus_calculator':
-                context_ = tool_call.calculus_calculator(expression=tool.function.arguments['expression'],
-                                                        operation=tool.function.arguments['operation'],
-                                                        evaluate=tool.function.arguments['evaluate'])
+                args = tool.function.arguments
+                context_ = tool_call.calculus_calculator(expression=args.get('expression'),
+                                                        operation=args.get('operation'),
+                                                        evaluate=args.get('evaluate'),
+                                                        variable=args.get('variable'),
+                                                        lower_bound=args.get('lower_bound'),
+                                                        upper_bound=args.get('upper_bound'))
             elif tool.function.name == 'search_web':
                 contexts = tool_call.web_search(query=tool.function.arguments['query'])
                 # print(contexts, type(contexts))
@@ -83,7 +87,7 @@ def smart_agent_decision(user_message: str):
         
         return user_message
         
-    return user_message
+    return [user_message]
 # Ví dụ sử dụng
 # if __name__ == "__main__":
 #     user_message = {"role": "user", "content": "Tôi muốn nghe bài hát The Night"}
